5031809e3bcbca61fdf0bbad3a6779a861e38501
Sage Ross <ragesoss@gmail.com>
2020-03-30 15:15:33 -0700

    Temporarily remove 'remove_index' line
    This one already ran successfully in staging.

--- db/migrate/20200327172134_convert_training_modules_to_utf8_mb4.rb
+++ db/migrate/20200327172134_convert_training_modules_to_utf8_mb4.rb
@@ -1,6 +1,6 @@
 class ConvertTrainingModulesToUtf8Mb4 < ActiveRecord::Migration[6.0]
   def change
-    remove_index :training_slides, :slug
+    # remove_index :training_slides, :slug
     execute "ALTER TABLE training_slides ROW_FORMAT=DYNAMIC"
     execute "ALTER TABLE training_slides CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
     execute "ALTER TABLE training_slides MODIFY content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
